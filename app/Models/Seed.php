a8a3978a1f66f3f584d35d5197b9e7724b39604a
asboldyrev <angelcorpc@ya.ru>
2023-02-25 01:00:30 +0700

    Добавил модель семян

---
+++ app/Models/Seed.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Seed extends Model
+{
+    use HasFactory, HasUuids;
+
+	protected $fillable = [
+		'name',
+		'manufacturer',
+		'description',
+		'bought_at',
+		'expiration_at',
+		'count',
+	];
+
+	protected $casts = [
+		'bought_at' => 'datetime',
+		'expiration_at' => 'datetime',
+		'count' => 'integer',
+	];
+
+	protected $primaryKey = 'uuid';
+
+	protected $incrementing = false;
+}
---
+++ database/migrations/2023_02_24_175242_create_seeds_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('seeds', function (Blueprint $table) {
+			$table->uuid()->primary();
+			$table->string('name');
+			$table->string('manufacturer')->nullable();
+			$table->text('description')->nullable();
+			$table->timestamp('bought_at')->nullable();
+			$table->timestamp('expiration_at')->nullable();
+			$table->unsignedSmallInteger('count')->default(0);
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('seeds');
+	}
+};
