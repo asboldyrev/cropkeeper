2cd9e5be79fadeb7d5f14cdeb0cf5fdc039927f3
asboldyrev <angelcorpc@ya.ru>
2023-03-04 21:52:11 +0700

    Добавил связь участка с огородом

--- app/Models/Garden.php
+++ app/Models/Garden.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Garden extends Model
 {
@@ -29,5 +30,10 @@ class Garden extends Model
 
 	public function users():BelongsToMany {
 		return $this->belongsToMany(User::class)->withPivot('role');
+	}
+
+
+	public function plots():HasMany {
+		return $this->hasMany(Plot::class);
 	}
 }
