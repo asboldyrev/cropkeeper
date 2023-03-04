5f0b00ba807766e13ffe2cc1d7d7ea888c2d4e70
asboldyrev <angelcorpc@ya.ru>
2023-03-04 21:07:00 +0700

    Переделал экшен проверки пользователя для возможности передавать коллекцию пользователей

--- app/Actions/CheckUserAuth.php
+++ app/Actions/CheckUserAuth.php
@@ -3,13 +3,27 @@
 namespace App\Actions;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class CheckUserAuth
 {
-	public function __invoke(User $user): bool
+	public function __invoke(User|Collection $user): bool
 	{
+		if($user instanceof Collection) {
+			foreach($user as $_user) {
+				$this->check($_user);
+			}
+
+			return true;
+		}
+
+		return $this->check($user);
+	}
+
+
+	protected function check(User $user) {
 		if($user->uuid == Auth::user()->uuid) {
 			return true;
 		}
