a7119990380861ed3bd0a8e4d3ccfd116542399c
asboldyrev <angelcorpc@ya.ru>
2023-03-12 01:28:30 +0700

    Добавил сидер фейковых данных

---
+++ database/seeders/FakeDataSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\PlantingMethod;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FakeDataSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void
+	{
+		$user = User::first();
+		$planting_method = PlantingMethod::first();
+
+		// Garden
+		$garden = $user->gardens()->create([
+			"name" => "Основной огород",
+			"description" => "тестовый огород",
+			"area" => 12.5
+		], [ 'role' => 'owner' ]);
+
+		// Plot
+		$plot = $garden->plots()->make([
+			"name" => "Грядка 1",
+			"area" => 5,
+			"ph" => 7
+		]);
+		$plot->plantingMethod()->associate($planting_method);
+		$plot->save();
+
+		// Seed
+		$seed = $garden->seeds()->create([
+			"name" => "Семена",
+			"count" => 10
+		]);
+
+		// Plant
+		$plant = $plot->plants()->make([
+			"name" => "Растение",
+			"is_transplanted" => true,
+		]);
+		$plant->seed()->associate($seed);
+		$plant->garden()->associate($garden);
+		$plant->save();
+	}
+}
