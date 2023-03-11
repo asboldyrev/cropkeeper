579dc3e8f33a17e07bbeccf20a728a5ddef13df8
asboldyrev <angelcorpc@ya.ru>
2023-03-11 22:29:09 +0700

    Удалил лишнюю строку в PlantingMethodController контроллере

--- app/Http/Controllers/Api/PlantingMethodController.php
+++ app/Http/Controllers/Api/PlantingMethodController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PlantingMethodResource;
 use App\Models\PlantingMethod;
-use Illuminate\Http\Request;
 
 class PlantingMethodController extends Controller
 {
