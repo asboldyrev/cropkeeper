3d96a46425b14814bd591416ffab11a0b78698aa
asboldyrev <angelcorpc@ya.ru>
2023-03-18 22:39:11 +0700

    Добавил компонент переключателя тем

---
+++ resources/js/Blocks/ThemeSwitcher.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="btn-group" role="group" aria-label="Basic example">
+		<input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" :checked="selectedTheme == 'light'" @click="toggleTheme('light')">
+		<label class="btn btn-outline-primary btn-sm" for="option1">
+			<i class="ri-sun-line"></i>
+		</label>
+
+		<input type="radio" class="btn-check" name="options" id="option2" autocomplete="off" :checked="selectedTheme == 'dark'" @click="toggleTheme('dark')">
+		<label class="btn btn-outline-primary btn-sm" for="option2">
+			<i class="ri-moon-line"></i>
+		</label>
+	</div>
+</template>
+
+<script setup>
+import { onBeforeMount, ref } from "vue";
+
+	const selectedTheme = ref(null);
+
+	function toggleTheme(themeName) {
+		document.querySelector('[data-bs-theme]').dataset.bsTheme = themeName
+		localStorage.setItem('theme', themeName)
+		selectedTheme.value = themeName
+	}
+
+
+	onBeforeMount(() => {
+		const themeName = localStorage.getItem('theme')
+		if(themeName) {
+			toggleTheme(themeName)
+		}
+	})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
