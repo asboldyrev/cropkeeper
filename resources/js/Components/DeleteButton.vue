1736f19048736bb1d93cc60761cd389202565ce9
asboldyrev <angelcorpc@ya.ru>
2023-03-20 00:07:57 +0700

    Добавил компоненты для кнопки с иконкой и кнопки удаления с подтверждением

---
+++ resources/js/Components/DeleteButton.vue
@@ -0,0 +1,40 @@
+<template>
+	<IconButton
+		type="danger"
+		:icon="icon"
+		:outline="outline"
+		@click="deleteConfirm"
+	>
+		<slot></slot>
+	</IconButton>
+</template>
+
+<script setup>
+	import IconButton from './IconButton.vue'
+
+	const props = defineProps({
+		confirmText: {
+			type: String,
+			default: 'Вы действительно хотите удалить данный элемент?'
+		},
+		outline: {
+			type: Boolean
+		},
+		icon: {
+			type: String
+		}
+	})
+
+	const emit = defineEmits([ 'confirm' ])
+
+	function deleteConfirm(event) {
+		if(!confirm(props.confirmText)) {
+			event.preventDefault();
+		} else {
+			emit('confirm', event)
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ resources/js/Components/IconButton.vue
@@ -0,0 +1,36 @@
+<template>
+	<button type="button" class="btn btn-sm" :class="buttonType">
+		<i v-if="icon" :class="icon"></i>
+		<slot />
+	</button>
+</template>
+
+<script setup>
+	import { computed } from "vue"
+
+	const props = defineProps({
+		outline: {
+			type: Boolean
+		},
+		type: {
+			type: String,
+			require: true
+		},
+		icon: {
+			type: String
+		}
+	})
+
+	const buttonType = computed(() => {
+		if(props.outline) {
+			return `btn-outline-${props.type}`
+		}
+
+		return `btn-${props.type}`
+	})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
