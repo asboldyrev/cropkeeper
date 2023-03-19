5d10745058e12e255970aa76f2fde3fe98d18ca0
asboldyrev <angelcorpc@ya.ru>
2023-03-20 00:09:19 +0700

    Добавил загрузку данных при "инициализации" приложения

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -3,8 +3,10 @@
 </template>
 
 <script setup>
-	import { computed, onBeforeMount, provide, ref } from "vue";
+	import { computed, onBeforeMount, provide, ref } from "vue"
+	import { useStore } from "./store"
 
+	const store = useStore()
 	const isMobile = ref(false)
 
 	provide('isMobile', isMobile)
@@ -19,7 +21,20 @@
 		doit = setTimeout(hasMobile, 100)
 	})
 
-	onBeforeMount(hasMobile)
+	async function loadData() {
+		let try_load = 0
+		// ждем, пока переменная myVariable не будет готова
+		while (!store.token && try_load < 10) {
+			await new Promise(resolve => setTimeout(resolve, 100)); // ждем 1 секунду
+			try_load++
+		}
+		store.syncGardens()
+	}
+
+	onBeforeMount(() => {
+		hasMobile()
+		loadData()
+	})
 </script>
 
 <style lang="scss" scoped>
