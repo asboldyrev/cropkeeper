03a37913e5546f5bcfcf21b228c7627fd10bb686
asboldyrev <angelcorpc@ya.ru>
2023-04-01 19:11:47 +0700

    Правки

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -5,8 +5,10 @@ import Login from '@/Pages/Login.vue'
 import Logout from '@/Pages/Logout.vue'
 import Plants from '@/Pages/Plants.vue'
 import Plots from '@/Pages/Plots.vue'
+import Profile from '@/Pages/Profile.vue'
 import Seeds from '@/Pages/Seeds.vue'
 
+import AccountLayout from '@/Layouts/AccountLayout.vue'
 import AppLayout from '@/Layouts/AppLayout.vue'
 import AuthLayout from '@/Layouts/AuthLayout.vue'
 
@@ -14,48 +16,92 @@ export default [
 	{
 		path: '/',
 		component: AppLayout,
+		name: 'app-layout',
 		children: [
 			{
 				path: '/',
 				component: Dashboard,
 				name: 'dashboard',
-				meta: { title: 'Сводка' }
+				meta: {
+					title: 'Сводка',
+					name: 'Сводка',
+					icon: 'ri-dashboard-line',
+				}
 			},
 			{
 				path: '/calendar',
 				component: Calendar,
 				name: 'calendar',
-				meta: { title: 'Календарь' }
+				meta: {
+					title: 'Календарь',
+					name: 'Календарь',
+					icon: 'ri-calendar-line',
+				}
 			},
 			{
 				path: '/plants',
 				component: Plants,
 				name: 'plants',
-				meta: { title: 'Растения' }
-			},
-			{
-				path: '/plots',
-				component: Plots,
-				name: 'plots',
-				meta: { title: 'Участок' }
+				meta: {
+					title: 'Растения',
+					name: 'Растения',
+					icon: 'ri-plant-line',
+				}
 			},
 			{
 				path: '/seeds',
 				component: Seeds,
 				name: 'seeds',
-				meta: { title: 'Семена' }
+				meta: {
+					title: 'Семена',
+					name: 'Семена',
+					icon: 'ri-seedling-line',
+				}
 			},
 			{
 				path: '/harvest',
 				component: Harvest,
 				name: 'harvest',
-				meta: { title: 'Урожай' }
+				meta: {
+					title: 'Урожай',
+					name: 'Урожай',
+					icon: 'ri-shopping-basket-line',
+				}
+			},
+			{
+				path: '/plots',
+				component: Plots,
+				name: 'plots',
+				meta: {
+					title: 'Участок',
+					name: 'Участок',
+					icon: 'ri-shape-line',
+				}
+			},
+		]
+	},
+
+	{
+		path: '/',
+		component: AccountLayout,
+		name: 'account-layout',
+		children: [
+			{
+				path: '/profile',
+				component: Profile,
+				name: 'profile',
+				meta: {
+					title: 'Профиль',
+					name: 'Профиль',
+					icon: 'ri-user-line',
+				}
 			},
 		]
 	},
 	{
 		path: '/',
 		component: AuthLayout,
+		name: 'auth-layout',
 		children: [
 			{
 				path: '/login',
