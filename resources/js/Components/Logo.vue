07a6fcfe913d3c0b22c72a90cd5b41a96bb91e17
asboldyrev <angelcorpc@ya.ru>
2023-04-01 19:08:30 +0700

    Добавил компонент логотипа

---
+++ resources/js/Components/Logo.vue
@@ -0,0 +1,12 @@
+<template>
+	<KeepAlive>
+		<router-link class="d-inline-block text-decoration-none text-body" :to="{ name: 'dashboard' }">
+			<img src="img/logo.svg" alt="Logo" height="28" class="ms-3">
+			<span class="ms-2">CropKeeper</span>
+		</router-link>
+	</KeepAlive>
+</template>
+
+<script setup>
+
+</script>
